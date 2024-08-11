77f476d83e5b08651cfc413f7c2674ab9b0b7a45
Aria-Hawa <shigototravailler@gmail.com>
2024-08-11 18:26:26 +0800

    0811-classifyTextArea

--- customizeJersey/js/jsAria.js
+++ customizeJersey/js/jsAria.js
@@ -2,7 +2,7 @@
 //styleInfo區域動態顯示
 let shapeAreaDiv = document.querySelector('#shapeArea');
 let designAreaDiv = document.querySelector('#designArea');
-// let textAreaDiv = document.querySelector('#textArea');
+let textAreaDiv = document.querySelector('#textArea');
 // let colorAreaDiv = document.querySelector('#colorArea');
 // let pictureAreaDiv = document.querySelector('#pictureArea');
 // let numberAreaDiv = document.querySelector('#numberArea');
@@ -10,14 +10,14 @@ let designAreaDiv = document.querySelector('#designArea');
 function displayNone() {
     shapeAreaDiv = document.querySelector('#shapeArea');
     designAreaDiv = document.querySelector('#designArea');
-    // textAreaDiv = document.querySelector('#textArea');
+    textAreaDiv = document.querySelector('#textArea');
     // colorAreaDiv = document.querySelector('#colorArea');
     // pictureAreaDiv = document.querySelector('#pictureArea');
     // numberAreaDiv = document.querySelector('#numberArea');
 
     shapeAreaDiv.style.display = 'none';
     designAreaDiv.style.display = 'none';
-    // preTextDiv.style.display = 'none';
+    textAreaDiv.style.display = 'none';
     // preColorDiv.style.display = 'none';
     // prePictureDiv.style.display = 'none';
     // preNumberDiv.style.display = 'none';
@@ -34,4 +34,41 @@ function displayShape() {
 function displayDesign() {
     displayNone();
     designAreaDiv.style.display = 'flex';
+}
+
+function displayText() {
+    displayNone();
+    textAreaDiv.style.display = 'flex';
+    // textArea2nd.style.display = 'none';
+    // textArea3rd.style.display = 'none';
+}
+
+//textArea步驟動態顯示
+let textArea1st = document.querySelector('#textArea-1st');
+let textArea2nd = document.querySelector('#textArea-2nd');
+let textArea3rd = document.querySelector('#textArea-3rd');
+
+function textStepDisplayNone() {
+    textArea1st = document.querySelector('#textArea-1st');
+    textArea2nd = document.querySelector('#textArea-2nd');
+    textArea3rd = document.querySelector('#textArea-3rd');
+
+    textArea1st.style.display = 'none';
+    textArea2nd.style.display = 'none';
+    textArea3rd.style.display = 'none';
+}
+
+function display1stStep() {
+    textStepDisplayNone();
+    textArea1st.style.display = 'flex';
+}
+
+function display2ndStep() {
+    textStepDisplayNone();
+    textArea2nd.style.display = 'flex';
+}
+
+function display3rdStep () {
+    textStepDisplayNone();
+    textArea3rd.style.display = 'flex';
 }
