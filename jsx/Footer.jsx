d96464d8b9031e04cba4550a043208571f67a109
Aria-Hawa <shigototravailler@gmail.com>
2024-09-02 13:05:14 +0800

    0902-修改小問題

--- jsx/Footer.jsx
+++ jsx/Footer.jsx
@@ -1,24 +1,31 @@
-function Header() {
+function Footer() {
     return(
-    <header id="fixedTopbar">
-        <div className="topbar">
-            <h1><a href="./index.html"><img src="./images/logo.png" alt="VSER客製化球衣" /></a></h1>
-            <div className="menuList">
-                <nav className="menuItem">
-                    <ul>
-                        <li><a href="#">品牌理念</a></li>
-                        <li><a href="#">專案成果</a></li>
-                        <li><a href="#">商品瀏覽</a></li>
-                        <li><a href="#">訂購說明</a></li>
-                        <li><a href="#">會員專區</a></li>
-                    </ul>
-                </nav>
-                <div className="rightList">
-                    <a href="#" className="customizeBtn">訂製專區</a>
-                    <a href="#" className="cartBtn"></a>
-                </div>
-            </div>
+        <footer>
+        <ul className="footerList">
+            <li className="footerItem">About
+                <ul>
+                    <li>關於VSER</li>
+                </ul>
+            </li>
+            <li className="footerItem">SERVICE
+                <ul>
+                    <li>條款與細則</li>
+                    <li>隱私權政策</li>
+                    <li>客製化政策</li>
+                </ul>
+            </li>
+            <li className="footerItem">CONTACT
+                <ul>
+                    <li>新北市汐止區新台五路二段77號15樓之3</li>
+                    <li>vser.tw@gmail.com</li>
+                    <li>02-86482203</li>
+                </ul>
+            </li>
+        </ul>
+        <div>
+            <figure><img src="./images/logo-2.svg" alt=""/></figure>
+            <small>&copy;2024 VSER, Inc. 版權所有</small>
         </div>
-    </header>
+    </footer>
 );
 }
--- jsx/Header.jsx
+++ jsx/Header.jsx
@@ -1,31 +1,24 @@
-function Footer() {
-    return(
-        <footer>
-        <ul className="footerList">
-            <li className="footerItem">About
-                <ul>
-                    <li>關於VSER</li>
-                </ul>
-            </li>
-            <li className="footerItem">SERVICE
-                <ul>
-                    <li>條款與細則</li>
-                    <li>隱私權政策</li>
-                    <li>客製化政策</li>
-                </ul>
-            </li>
-            <li className="footerItem">CONTACT
-                <ul>
-                    <li>新北市汐止區新台五路二段77號15樓之3</li>
-                    <li>vser.tw@gmail.com</li>
-                    <li>02-86482203</li>
-                </ul>
-            </li>
-        </ul>
-        <div>
-            <figure><img src="./images/logo-2.svg" alt=""/></figure>
-            <small>&copy;2024 VSER, Inc. 版權所有</small>
-        </div>
-    </footer>
-);
+function Header() {
+    return (
+        <header id="fixedTopbar">
+            <div className="topbar">
+                <h1><a href="./index.html"><img src="./images/logo.png" alt="VSER客製化球衣" /></a></h1>
+                <div className="menuList">
+                    <nav className="menuItem">
+                        <ul>
+                            <li><a href="./brandConcept.html">品牌理念</a></li>
+                            <li><a href="./pojectResults.html">專案成果</a></li>
+                            <li><a href="./productBrowsing.html">商品瀏覽</a></li>
+                            <li><a href="./Customization_process.html">訂購說明</a></li>
+                            <li><a href="#">會員專區</a></li>
+                        </ul>
+                    </nav>
+                    <div className="rightList">
+                        <a href="#" className="customizeBtn">訂製專區</a>
+                        <a href="#" className="cartBtn"></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
