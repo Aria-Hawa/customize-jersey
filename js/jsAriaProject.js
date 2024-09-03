3ee15d4a2c81a94563b17d3399d538feee4df2c1
Aria-Hawa <shigototravailler@gmail.com>
2024-09-03 17:17:21 +0800

    0903-chooseProject項目刪除/新增完善

--- js/jsAriaProject.js
+++ js/jsAriaProject.js
@@ -1,16 +1,6 @@
 // 使用jQuery
 $(function () {
-    // 共用
-    // .arrow的互動
-    // $('.arrow').click(function () {
-    //     // .arrow旋轉
-    //     $(this).toggleClass('beClick');
-    //     // 新增.show類別
-    //     $(this).closest('.table').find('table').toggleClass('show');
-    //     $(this).closest('.tableCaption').toggleClass('show');
-    // });
-
-    $('.tableCaption').click(function () {
+    $(document).on('click', '.tableCaption', function () {
         // .arrow旋轉
         $(this).find('.arrow').toggleClass('beClick');
         // 新增.show類別
@@ -18,37 +8,19 @@ $(function () {
         $(this).closest('.tableCaption').toggleClass('show');
     });
 
-    // table表格新增件數欄位的hover
-    $('.tableFoot').hover(
-        // 滑鼠移入時候
-        function () {
-            $(this).css('cursor', 'pointer');
-            $('.addBtn').css('background-color', 'var(--Orange)');
-            $('.tableFoot p').css('color', 'var(--Orange)');
-        },
-        // 滑鼠移出時候
-        function () {
-            // $(this).css('cursor', 'pointer');
-            $('.addBtn').css('background-color', 'white');
-            $('.tableFoot p').css('color', 'white');
-        });
-
     // 當table表格新增件數欄位click時
-    $('.tableFoot').click(function () {
-        // 更改css樣式
-        $('.addBtn').css('background-color', 'var(--Darkorange)');
-        $('.tableFoot p').css('color', 'var(--Darkorange)');
+
+    $(document).on('click', '.tableFoot', function () {
         // 取得<table>和<tbody>
         let getTable = $(this).closest('table');
         let getTbody = $(getTable).find('tbody:last');
-        // 複製<tbody> =>Chatgpt
-        // 新增一個tbody
+        // 建立變數儲存 複製後的<tbody>
         let newTbody = $(getTbody).clone();
         // 清空<tobdy>的內容 =>Chatgpt
         $(newTbody).find('input').val('');
         $(newTbody).find('select').find('option').val('');
         $(newTbody).find('.itemNo').text('');
-        // 不是tbody被appen，這樣的結果是tbody裡面有tbody
+        // 不是tbody被append，這樣的結果是tbody裡面有tbody
         // impossible!!!是因為我有設定tfoot，所以就算tbody放在tfoot下面也不會跑版?!
         $(getTable).append(newTbody);
         updateItemNo(getTable);
@@ -61,12 +33,29 @@ $(function () {
     $(document).on('click', '.delBtn', function () {
         // 在刪除之前先儲存del的table
         let delClosestTable = $(this).closest('table');
-        // 變數存起來之後再刪除tbody
-        $(this).closest('tbody').remove();
-        updateItemNo(delClosestTable);
+        let countItem = $(delClosestTable).find('tbody').length;
+        let projectImg = $(delClosestTable).closest('.table').find('.captionLeft figure').css('background-image').match(/url\(["']?([^"']*)["']?\)/)[1];
+        let projectName = $(delClosestTable).closest('.table').find('.captionLeft p').text();
+        let projectPrice = $(delClosestTable).closest('.table').find('.price').text();
+        if ($(delClosestTable).find('tbody').length == 1) {
+            if (confirm(`確定要刪除<${projectName}>項目嗎?`) == true) {
+                let newUserDesign = $('#emptyUserDesign').clone();
+                $(newUserDesign).find('img').attr('src',projectImg);
+                $(newUserDesign).find('h3').text(projectName);
+                $(newUserDesign).find('span').text(projectPrice);
+                $(newUserDesign).attr('id','');
+                $('#innerContent').find('.userDesign:last').after(newUserDesign);
+                $(newUserDesign).css('display','flex');
+                $(this).closest('tbody').closest('.table').remove();
+            } else {
+                alert(`取消刪除<${projectName}>項目`);
+            };
+        } else {
+            // 變數存起來之後再刪除tbody
+            $(this).closest('tbody').remove();
+            updateItemNo(delClosestTable);
+        };
     });
-
-
     // 把「項次」隨著tbody變更獨立為一個待呼叫的函示
     function updateItemNo(thisTable) {
         $(thisTable).find('tbody').each(function (index) {
@@ -74,9 +63,7 @@ $(function () {
         })
     }
 
-
-
-    // 8/31 測試popupe功能
+    // 8/31 測試popup功能
     // 當需要顯示彈出視窗時
     $('#addStyle').on('click', function () {
         $('#popup').fadeIn();
@@ -84,10 +71,39 @@ $(function () {
 
     // 當需要關閉彈出視窗時
     $('#closePopup').on('click', function () {
-        console.log('觸發成功')
         $('#popup').fadeOut();
     });
 
+    // popup內的userDesign click時，標記selected
+    $(document).on('click','.userDesign',function () {
+        $('.userDesign').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // 按下確認鍵後
+    $('#chekBtn').click(function () {
+        // 取得projectname
+        let projectName = $('.userDesign.selected').find('h3').text();
+        // 取得img路徑
+        let ImgSrc = $('.userDesign.selected').find('img').attr('src');
+        // 取得單價
+        let unitPrice = $('.userDesign.selected').find('span').text();
+        // clone .table
+        let newProject = $('#project').clone();
+        $(newProject).find('.captionLeft p').text(projectName);
+        $(newProject).find('.captionRight span').text(unitPrice);
+        $(newProject).find('.captionLeft figure').css('background-image', `url(${ImgSrc})`);
+        $(newProject).css('display', 'block');
+        $(newProject).attr('id','');
+        $('.table:last').after(newProject);
+        // 被選取的project要移除
+        $('.userDesign.selected').remove();
+    });
+
+    // 整筆總額隨project變更
+    $(document).find('.price').each((index,price)=>{
+        console.log(price.innerText);
+    });
 
 });
 
